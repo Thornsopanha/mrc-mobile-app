9507fb4069c6674fdaa6f4935b2184184bf7b95e
Thea Choem <theacheng.g6@gmail.com>
2023-03-28 23:47:16 +0700

    setup firebase core

--- mrc_mobile_app/lib/main.dart
+++ mrc_mobile_app/lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mrc_mobile_app/firebase_options.dart';
 import 'package:mrc_mobile_app/homepage.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
